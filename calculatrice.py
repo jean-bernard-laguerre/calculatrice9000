d82aa2166f5e406b157fa62a3538c798b5cca1c2
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2023-01-19 13:42:56 +0100

    bouton factoriel, inverse, mod, puissance

--- calculatrice.py
+++ calculatrice.py
@@ -16,7 +16,7 @@ def bouton_num(num):
 def calcul():
 
     global equation
- 
+
     try:
         equation += ecran.get()
         resultat = eval(equation)
@@ -50,11 +50,31 @@ def operation(operateur):
         case "%":
             equation_ecran.set(equation_ecran.get() + ecran.get() + "%")
             equation += ecran.get() +"/100"
+        case "1/x":
+            equation_ecran.set(equation_ecran.get() + f"(1/{ecran.get()})")
+            equation += f"(1/{ecran.get()})"
+        case "!":
+            equation_ecran.set(equation_ecran.get() + ecran.get() + "!")
+            equation += str(factoriel( int(ecran.get()) ))
+        case "^":
+            equation_ecran.set(equation_ecran.get() + ecran.get() + "^")
+            equation += ecran.get() + "**"
+        case "mod":
+            equation_ecran.set(equation_ecran.get() + ecran.get() + "mod")
+            equation += ecran.get() + "%"
         case _:
             equation_ecran.set(equation_ecran.get() + ecran.get() + operateur)
             equation += ecran.get() + operateur
 
     ecran.delete(0, END)
+
+
+def factoriel(num):
+
+    if num == 1:
+        return 1
+    else:
+        return num * factoriel(num-1)
 
 
 def effacer():
@@ -66,15 +86,15 @@ def effacer():
     equation = ""
 
 
-def clear_historique():
-    for widget in hist.winfo_children():
-        widget.destroy()
+def suppr_historique():
+    for element in hist.winfo_children():
+        element.destroy()
 
 
 
 ecran_eq = Label(fenetre, height=3, bg="white", font=('Arial',12), textvariable=equation_ecran)
 ecran_eq.pack(fill="both", expand="yes")
-ecran = Entry(fenetre, font=('Arial',16), highlightthickness=0, relief=FLAT)
+ecran = Entry(fenetre, font=('Arial',18), highlightthickness=0, relief=FLAT, justify=("right"))
 ecran.pack(fill="both", expand="yes")
 
 touches = Frame(fenetre)
@@ -82,7 +102,7 @@ touches.pack(anchor= "center")
 
 calc_hist = LabelFrame(fenetre, text="Historique")
 calc_hist.pack(fill="both", expand="yes")
-btn_vide_hist = Button(calc_hist, text="Vider l'historique", command= clear_historique)
+btn_vide_hist = Button(calc_hist, text="Vider l'historique", command= suppr_historique)
 btn_vide_hist.pack(fill="both", expand="yes")
 hist = Frame(calc_hist)
 hist.pack(fill="both", expand="yes")
@@ -110,36 +130,44 @@ btn_carré = Button(touches, width=7, height=2, text="²", command=lambda : oper
 btn_pourcent = Button(touches, width=7, height=2, text="%", command=lambda : operation("%"), font=('Arial',12))
 
 btn_fact = Button(touches, width=7, height=2, text="x!", command=lambda : operation("!"), font=('Arial',12))
-
+btn_inv = Button(touches, width=7, height=2, text="1/x", command=lambda : operation("1/x"), font=('Arial',12))
+btn_puissance = Button(touches, width=7, height=2, text="^", command=lambda : operation("^"), font=('Arial',12))
+btn_mod = Button(touches, width=7, height=2, text="mod", command=lambda : operation("mod"), font=('Arial',12))
 
 btn_suppr = Button(touches, width=7, height=2, text="suppr", command= effacer, bg="red", fg="white", font=('Arial',12))
 
 btn_egal = Button(touches, width=7, height=2, text="=", command=calcul, bg="orange", font=('Arial',12))
 
 
-btn_1.grid(row=1, column=1)
-btn_2.grid(row=1, column=2)
-btn_3.grid(row=1, column=3)
-btn_4.grid(row=2, column=1)
-btn_5.grid(row=2, column=2)
-btn_6.grid(row=2, column=3)
-btn_7.grid(row=3, column=1)
-btn_8.grid(row=3, column=2)
-btn_9.grid(row=3, column=3)
-btn_0.grid(row=4, column=2)
-btn_point.grid(row=4, column=1)
 
-btn_plus.grid(row=1, column=0)
-btn_moins.grid(row=2, column=0)
-btn_mult.grid(row=3, column=0)
-btn_div.grid(row=4, column=0)
+btn_1.grid(row=2, column=1)
+btn_2.grid(row=2, column=2)
+btn_3.grid(row=2, column=3)
+btn_4.grid(row=3, column=1)
+btn_5.grid(row=3, column=2)
+btn_6.grid(row=3, column=3)
+btn_7.grid(row=4, column=1)
+btn_8.grid(row=4, column=2)
+btn_9.grid(row=4, column=3)
+btn_0.grid(row=5, column=2)
+btn_point.grid(row=5, column=1)
+
+btn_plus.grid(row=2, column=0)
+btn_moins.grid(row=3, column=0)
+btn_mult.grid(row=4, column=0)
+btn_div.grid(row=5, column=0)
 
 btn_racine.grid(row=0, column=0)
 btn_carré.grid(row=0, column=1)
 btn_pourcent.grid(row=0, column=2)
 btn_suppr.grid(row=0, column=3)
 
-btn_egal.grid(row=4, column=3)
+btn_fact.grid(row=1,column=0)
+btn_inv.grid(row=1,column=1)
+btn_puissance.grid(row=1, column=2)
+btn_mod.grid(row=1, column=3)
+
+btn_egal.grid(row=5, column=3)
 
 
 fenetre.mainloop()
